39b5e8a98cd197dd6c5e1f1067901b6fc73fa4b3
Daniel Mitrache <mitracheadaniel@gmail.com>
2025-10-18 14:30:17 +0300

    commit

--- backend/backend/Controllers/BasicController.cs
+++ backend/backend/Controllers/BasicController.cs
@@ -8,5 +8,15 @@ public class BasicController : ControllerBase
     [HttpGet]
     [HttpGet("/test")]
     public IActionResult Get() => Ok(new { message = "Hello World!" });
-    
+
+}
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class GrasuController : ControllerBase
+{
+    [HttpGet]
+    [HttpGet("/grasu")]
+    public IActionResult Get() => Ok(new { message = "Grasu e cel mai tare!" });
 }
